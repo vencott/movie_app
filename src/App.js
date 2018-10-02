3b19422882fa7b7ba5ce70bb9b54182801a5a464
Jungmin Kim <vencott@applepie4.com>
2018-10-02 17:42:46 +0900

    4-3. Loading States

--- src/App.js
+++ src/App.js
@@ -3,48 +3,48 @@ import './App.css';
 import Movie from './Movie';
 
 class App extends Component {
-    state = {
-        movies: [
-            {
-                title: "Matrix",
-                poster: "https://is1-ssl.mzstatic.com/image/thumb/Video115/v4/a2/ff/49/a2ff498c-2567-39f3-578f-4f023d414fbd/pr_source.lsr/268x0w.png"
-            },
-            {
-                title: "Full Metal Jacket",
-                poster: "https://upload.wikimedia.org/wikipedia/en/thumb/9/99/Full_Metal_Jacket_poster.jpg/220px-Full_Metal_Jacket_poster.jpg"
-            },
-            {
-                title: "Old boy",
-                poster: "https://upload.wikimedia.org/wikipedia/en/thumb/6/67/Oldboykoreanposter.jpg/220px-Oldboykoreanposter.jpg"
-            },
-            {
-                title: "Star wars",
-                poster: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQFJZ4cb20ESPz7NdrGsLKZH628KITeoz97CfutioMW56Qj2vMq"
-            }
-        ]
-    };
+    state = {};
 
     componentDidMount() {
         setTimeout(() => {
             this.setState({
                 movies: [
-                    ...this.state.movies,
+                    {
+                        title: "Matrix",
+                        poster: "https://is1-ssl.mzstatic.com/image/thumb/Video115/v4/a2/ff/49/a2ff498c-2567-39f3-578f-4f023d414fbd/pr_source.lsr/268x0w.png"
+                    },
+                    {
+                        title: "Full Metal Jacket",
+                        poster: "https://upload.wikimedia.org/wikipedia/en/thumb/9/99/Full_Metal_Jacket_poster.jpg/220px-Full_Metal_Jacket_poster.jpg"
+                    },
+                    {
+                        title: "Old boy",
+                        poster: "https://upload.wikimedia.org/wikipedia/en/thumb/6/67/Oldboykoreanposter.jpg/220px-Oldboykoreanposter.jpg"
+                    },
+                    {
+                        title: "Star wars",
+                        poster: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQFJZ4cb20ESPz7NdrGsLKZH628KITeoz97CfutioMW56Qj2vMq"
+                    },
                     {
                         title: "Transpotting",
                         poster: "https://upload.wikimedia.org/wikipedia/en/7/71/Trainspotting_ver2.jpg"
                     }
                 ]
             });
-        }, 1000);
+        }, 5000);
     }
 
+    _renderMovies = () => {
+        const movies = this.state.movies.map((movie, index) => {
+            return <Movie title={movie.title} poster={movie.poster} key={index}/>;
+        });
+        return movies;
+    };
+
     render() {
-        console.log("render");
         return (
             <div className="App">
-                {this.state.movies.map((movie, index) => {
-                    return <Movie title={movie.title} poster={movie.poster} key={index}/>;
-                })}
+                {this.state.movies ? this._renderMovies() : 'Loading'}
             </div>
         );
     }
