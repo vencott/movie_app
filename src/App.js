c834b46361701692c36d856dbfcbbd6b489db140
Jungmin Kim <vencott@applepie4.com>
2018-10-04 16:00:52 +0900

    6-3. Async Await in React

--- src/App.js
+++ src/App.js
@@ -6,15 +6,26 @@ class App extends Component {
     state = {};
 
     componentDidMount() {
-        fetch('https://yts.am/api/v2/list_movies.json?sort_by=rating')
-            .then(response => response.json())
-            .then(json => console.log(json))
-            .catch(err => console.log(err))
+        this._getMovies();
     }
 
+    _getMovies = async () => {
+        const movies = await this._callApi();
+        this.setState({
+            movies: movies
+        })
+    };
+
+    _callApi = () => {
+        return fetch('https://yts.am/api/v2/list_movies.json?sort_by=rating')
+            .then(response => response.json())
+            .then(json => json.data.movies)
+            .catch(err => console.log(err))
+    };
+
     _renderMovies = () => {
-        const movies = this.state.movies.map((movie, index) => {
-            return <Movie title={movie.title} poster={movie.poster} key={index}/>;
+        const movies = this.state.movies.map(movie => {
+            return <Movie title={movie.title} poster={movie.large_cover_image} key={movie.id}/>;
         });
         return movies;
     };
