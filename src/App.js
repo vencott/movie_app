603448d088e9e3b19f25243d2e9be3f66f765a3c
Jungmin Kim <vencott@applepie4.com>
2018-10-04 15:33:34 +0900

    6-2. Promises

--- src/App.js
+++ src/App.js
@@ -6,7 +6,10 @@ class App extends Component {
     state = {};
 
     componentDidMount() {
-        console.log(fetch('https://yts.am/api/v2/list_movies.json?sort_by=rating'));
+        fetch('https://yts.am/api/v2/list_movies.json?sort_by=rating')
+            .then(response => response.json())
+            .then(json => console.log(json))
+            .catch(err => console.log(err))
     }
 
     _renderMovies = () => {
